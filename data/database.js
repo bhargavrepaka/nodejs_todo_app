a11d796c0a14868a56303f78b0e74c32a16e5cfa
Repaka Bhargav <repaka.bhargav@gmail.com>
2023-07-19 01:09:01 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 export const connectDb=()=>{
     mongoose.connect(process.env.MONGO_URI,{
         dbName:"backendApi"
-    }).then(c=>console.log("Database Connected"))
+    }).then(c=>console.log(`Database Connected with ${c.connection.host}`))
     .catch(err=>console.log(err))
 }
