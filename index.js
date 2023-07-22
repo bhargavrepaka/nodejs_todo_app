0d23b53da459ef78a54bd066c080ff4bcec320e5
Repaka Bhargav <repaka.bhargav@gmail.com>
2023-07-22 23:17:09 +0530

    server uri

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:[process.env.FRONTEND_URL,"http://localhost:5173","http://localhost:5177"],
+    origin:["*","http://localhost:5173"],
     methods:['GET','POST','PUT','DELETE'],
     credentials:true
 }))
