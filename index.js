93a086e6c40f4e6d39c081aeee22b7adab714a5c
Repaka Bhargav <repaka.bhargav@gmail.com>
2023-07-22 22:56:47 +0530

    fixed frontend env error

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-    origin:[process.end.FRONTEND_URL,"http://localhost:5173","http://localhost:5177"],
+    origin:[process.env.FRONTEND_URL,"http://localhost:5173","http://localhost:5177"],
     methods:['GET','POST','PUT','DELETE'],
     credentials:true
 }))
